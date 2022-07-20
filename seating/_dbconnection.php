5e0686ec0d75dd58b883c28d3c9e1b90d2373a3f
Markus Zeller <git@markuszeller.com>
2022-07-20 14:37:46 +0200

    Format to no indentation. Do not use PHP close tags at end of file!

--- seating/_dbconnection.php
+++ seating/_dbconnection.php
@@ -1,13 +1,12 @@
 <?php
-    $host = 'localhost';
-    $user = 'seating';
-    $password = 'seating';
-    $db_Name = 'exam';
+$host = 'localhost';
+$user = 'seating';
+$password = 'seating';
+$db_Name = 'exam';
 
-    $con = mysqli_connect($host, $user, $password, $db_Name);
+$con = mysqli_connect($host, $user, $password, $db_Name);
 
-    if (mysqli_connect_errno())
-    {
-        echo "Failed to connect to database." . mysqli_connect_errno();
-    }
-?>
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to database." . mysqli_connect_errno();
+}
